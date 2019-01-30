c38fac90fe1ad276d87851b536d0da88b78903c2
Vipul Chaudhary <vpul.chaudhary@gmail.com>
2019-01-30 19:42:10 +0545

    car partially complete, moving

--- moving_car.cpp
+++ moving_car.cpp
@@ -1,10 +1,23 @@
 #include <graphics.h>
 
-int main()
-{
+void car(int i) {
+    rectangle(5+i,366,120+i,385);
+    line(31+i, 366, 38+i, 345);
+    line(38+i, 345, 86+i, 345);
+    line(94+i, 366, 86+i, 345);
+    circle(33+i, 385, 8);
+    circle(90+i, 385, 8);
+}
+
+int main() {
    int gd = DETECT, gm;
    initgraph(&gd,&gm,NULL);
-   line(0, 400, 800,400);
+   for(int i=0; i<520; i++) {
+       cleardevice();
+       line(0, 393, 800,393); //road
+       car(i);
+       delay(1);
+   }
    getch();
    closegraph();
    return 0;
