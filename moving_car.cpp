c6ca1bdc687224da776c90a7995169ede3bf8f0f
Vipul Chaudhary <vpul.chaudhary@gmail.com>
2019-01-30 20:47:49 +0545

    rim animation

--- moving_car.cpp
+++ moving_car.cpp
@@ -1,11 +1,22 @@
 #include <graphics.h>
 
 void car(int i) {
+    //car outline
     rectangle(5+i,366,120+i,385);
     line(31+i, 366, 38+i, 345);
     line(38+i, 345, 86+i, 345);
     line(94+i, 366, 86+i, 345);
     line(61+i, 366, 61+i, 345);
+    //moving rim animation
+    pieslice(33+i,385,(0+i)%360,(25+i)%360, 8);
+    pieslice(33+i,385,(90+i)%360,(115+i)%360, 8);
+    pieslice(33+i,385,(180+i)%360,(205+i)%360, 8);
+    pieslice(33+i,385,(270+i)%360,(295+i)%360, 8);
+    pieslice(90+i,385,(0+i)%360,(25+i)%360, 8);
+    pieslice(90+i,385,(90+i)%360,(115+i)%360, 8);
+    pieslice(90+i,385,(180+i)%360,(205+i)%360, 8);
+    pieslice(90+i,385,(270+i)%360,(295+i)%360, 8);
+    //wheels;
     circle(33+i, 385, 8);
     circle(90+i, 385, 8);
 }
