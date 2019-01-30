3149a349a8aa8ba3ebd4940c79385780a7409e73
Vipul Chaudhary <vpul.chaudhary@gmail.com>
2019-01-30 20:08:45 +0545

    windows

--- moving_car.cpp
+++ moving_car.cpp
@@ -5,6 +5,7 @@ void car(int i) {
     line(31+i, 366, 38+i, 345);
     line(38+i, 345, 86+i, 345);
     line(94+i, 366, 86+i, 345);
+    line(61+i, 366, 61+i, 345);
     circle(33+i, 385, 8);
     circle(90+i, 385, 8);
 }
